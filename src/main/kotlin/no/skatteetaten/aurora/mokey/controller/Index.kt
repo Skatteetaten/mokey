8b4041bb071a6722bec2aef9867e19f51c902ebb
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2017-09-21 15:35:43 +0200

    Fixed doc redirect issue

--- src/main/kotlin/no/skatteetaten/aurora/mokey/controller/Index.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/controller/Index.kt
@@ -1,10 +1,10 @@
 package no.skatteetaten.aurora.mokey.controller
 
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 @RequestMapping(value = "/")
 class IndexController {
 
