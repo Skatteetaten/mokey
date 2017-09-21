394c2ac824aecdeb357cd08a595cd432f76e4798
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2017-09-21 15:16:00 +0200

    Verifying route existence by checking with OpenShift

--- src/main/kotlin/no/skatteetaten/aurora/mokey/service/RouteService.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/service/RouteService.kt
@@ -1,14 +1,16 @@
 package no.skatteetaten.aurora.mokey.service
 
+import no.skatteetaten.aurora.mokey.service.openshift.OpenShiftResourceClient
 import org.springframework.stereotype.Component
-import org.springframework.web.client.RestTemplate
 
 data class Route(val namespace: String, val name: String)
 
 @Component
-class RouteService(val restTemplate: RestTemplate) {
+class RouteService(val openShiftResourceClient: OpenShiftResourceClient) {
 
     fun findRoute(namespace: String, name: String): Route? {
-        return Route(namespace, name)
+
+        val responseEntity = openShiftResourceClient.get("/namespaces/${namespace}/routes/${name}")
+        return responseEntity?.let { Route(namespace, name) }
     }
 }
---
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/service/exceptions.kt
@@ -0,0 +1,7 @@
+package no.skatteetaten.aurora.mokey.service
+
+abstract class ServiceException(message: String?, cause: Throwable?) : RuntimeException(message, cause) {
+    constructor(message: String) : this(message, null)
+}
+
+class OpenShiftException(messages: String?, cause: Throwable?) : ServiceException(messages, cause)
