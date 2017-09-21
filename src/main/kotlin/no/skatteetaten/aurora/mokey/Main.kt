a96aeaaa196606aab47d29c0440edee820a574a5
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2017-09-21 10:11:18 +0200

    Initial commit with gradle 4.1, spring-boot 1.5.7 and kotlin 1.1.4-3

---
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/Main.kt
@@ -0,0 +1,13 @@
+package no.skatteetaten.aurora.mokey
+
+import no.skatteetaten.aurora.annotations.AuroraApplication
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@AuroraApplication
+@SpringBootApplication
+class Main
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Main::class.java, *args)
+}
