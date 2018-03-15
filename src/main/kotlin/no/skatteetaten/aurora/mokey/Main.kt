057be2b7c12e1b8c1bd5e2086b101d5a5bc17413
Espen Strømland <espen.stromland@skatteetaten.no>
2018-03-15 11:14:15 +0100

    implemented commandlinerunner

--- src/main/kotlin/no/skatteetaten/aurora/mokey/Main.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/Main.kt
@@ -5,11 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.fabric8.openshift.client.DefaultOpenShiftClient
 import io.fabric8.openshift.client.OpenShiftClient
+import no.skatteetaten.aurora.mokey.service.AuroraApplicationCacheService
 import okhttp3.OkHttpClient
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -18,9 +21,8 @@ import java.security.KeyManagementException
 import java.security.NoSuchAlgorithmException
 import java.util.concurrent.TimeUnit
 
-@SpringBootApplication
-@EnableScheduling
-class Main {
+@Configuration
+class ApplicationConfig {
     @Bean
     @Primary
     fun mapper(): ObjectMapper {
@@ -42,12 +44,21 @@ class Main {
     private fun createRequestFactory(readTimeout: Long, connectionTimeout: Long): OkHttp3ClientHttpRequestFactory {
 
         val okHttpClientBuilder = OkHttpClient().newBuilder()
-                .readTimeout(readTimeout, TimeUnit.SECONDS)
-                .connectTimeout(connectionTimeout, TimeUnit.SECONDS)
+            .readTimeout(readTimeout, TimeUnit.SECONDS)
+            .connectTimeout(connectionTimeout, TimeUnit.SECONDS)
 
 
         return OkHttp3ClientHttpRequestFactory(okHttpClientBuilder.build())
     }
+}
+
+@SpringBootApplication
+@EnableScheduling
+class Main(val applicationCacheService: AuroraApplicationCacheService) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        applicationCacheService.load(listOf("aurora"))
+    }
+
 }
 
 
