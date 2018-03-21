7b94352a453fffb740ca6231e54ca669ed6e109e
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2018-03-21 10:55:47 +0100

    Test for CachedApplicationDataServiceTest

---
+++ src/test/kotlin/no/skatteetaten/aurora/mokey/service/CachedApplicationDataServiceTest.kt
@@ -0,0 +1,45 @@
+package no.skatteetaten.aurora.mokey.service
+
+import no.skatteetaten.aurora.mokey.controller.AuroraStatus
+import no.skatteetaten.aurora.mokey.controller.AuroraStatusLevel
+import no.skatteetaten.aurora.mokey.model.ApplicationData
+import no.skatteetaten.aurora.mokey.model.DeployDetails
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.mock
+
+class CachedApplicationDataServiceTest {
+
+    val app1Id = "some_id"
+    val app1v1 = ApplicationData(
+            app1Id,
+            AuroraStatus(AuroraStatusLevel.HEALTHY, ""),
+            "default",
+            "testapp",
+            "aurora",
+            "aurora",
+            deployDetails = DeployDetails("Complete", 1, 1)
+    )
+    val app1v2 = app1v1.copy(deployTag = "prod")
+
+    val sourceApplicationDataService = mock(ApplicationDataService::class.java)
+    val applicationDataService = CachedApplicationDataService(sourceApplicationDataService)
+
+    @Test
+    fun `should update cache from OpenShiftApplicationDataService`() {
+        val affiliations = listOf("aurora")
+
+        given(sourceApplicationDataService.findAllApplicationData(affiliations))
+                .willReturn(listOf(app1v1))
+                .willReturn(listOf(app1v2))
+
+        assertThat(applicationDataService.findApplicationDataById(app1Id)).isNull()
+
+        applicationDataService.refreshCache(affiliations)
+        assertThat(applicationDataService.findApplicationDataById(app1Id)).isEqualTo(app1v1)
+
+        applicationDataService.refreshCache(affiliations)
+        assertThat(applicationDataService.findApplicationDataById(app1Id)).isEqualTo(app1v2)
+    }
+}
