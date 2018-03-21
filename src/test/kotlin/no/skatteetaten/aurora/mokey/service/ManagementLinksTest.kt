4dda0c39091e9937a1fbd69899043d4a98a22919
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2018-03-21 09:55:53 +0100

    More tests

--- src/test/kotlin/no/skatteetaten/aurora/mokey/service/ManagementLinksTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/mokey/service/ManagementLinksTest.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import org.assertj.core.api.Assertions.assertThat
 import org.intellij.lang.annotations.Language
 import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertThrows
 
 class ManagementLinksTest {
 
@@ -28,5 +29,27 @@ class ManagementLinksTest {
 
         assertThat(managementLinks.linkFor(Endpoint.HEALTH)).isEqualTo("http://localhost:8081/health")
         assertThat(managementLinks.linkFor(Endpoint.INFO)).isEqualTo("http://localhost:8081/info")
+    }
+
+    @Test
+    fun `should fail when link is missing`() {
+        @Language("JSON")
+        val json = """{
+  "_links": {
+    "env": {
+      "href": "http://localhost:8081/env"
+    },
+    "info": {
+      "href": "http://localhost:8081/info"
+    }
+  }
+}"""
+        val managementLinks = ManagementLinks.parseManagementResponse(jacksonObjectMapper().readValue(json))
+
+        val e = assertThrows(ManagementEndpointException::class.java) {
+            managementLinks.linkFor(Endpoint.HEALTH)
+        }
+        assertThat(e.endpoint).isEqualTo(Endpoint.HEALTH)
+        assertThat(e.errorCode).isEqualTo("LINK_MISSING")
     }
 }
