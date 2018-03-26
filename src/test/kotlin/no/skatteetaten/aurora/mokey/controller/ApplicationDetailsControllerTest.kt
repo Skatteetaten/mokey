53fde43b9f9fc140e580c1f29d187b45c0d5299c
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2018-03-26 16:28:31 +0200

    Using testDataBuilders

--- src/test/kotlin/no/skatteetaten/aurora/mokey/controller/ApplicationDetailsControllerTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/mokey/controller/ApplicationDetailsControllerTest.kt
@@ -1,13 +1,9 @@
 package no.skatteetaten.aurora.mokey.controller
 
-import com.fasterxml.jackson.databind.node.MissingNode
 import no.skatteetaten.aurora.mokey.AbstractSecurityControllerTest
+import no.skatteetaten.aurora.mokey.PodDetailsDataBuilder
 import no.skatteetaten.aurora.mokey.model.*
 import no.skatteetaten.aurora.mokey.service.ApplicationDataService
-import no.skatteetaten.aurora.mokey.service.HealthResponse
-import no.skatteetaten.aurora.mokey.service.HealthStatus
-import no.skatteetaten.aurora.mokey.service.ManagementLinks
-import no.skatteetaten.aurora.utils.Right
 import org.junit.jupiter.api.Test
 import org.mockito.BDDMockito.given
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient
@@ -37,16 +33,7 @@ class ApplicationDetailsControllerTest : AbstractSecurityControllerTest() {
                 "namespace",
                 "affiliation",
                 deployDetails = DeployDetails("Complete", 1, 1),
-                pods = listOf(
-                        PodDetails(
-                                OpenShiftPodExcerpt("pod-1", "OK", 0, true, "10.0.0.1", "", null),
-                                Right(ManagementData(
-                                        ManagementLinks(emptyMap()),
-                                        Right(MissingNode.getInstance()),
-                                        Right(HealthResponse(HealthStatus.UP)),
-                                        Right(MissingNode.getInstance())
-                                ))
-                        ))
+                pods = listOf(PodDetailsDataBuilder().build())
         )
 
         given(applicationDataService.findApplicationDataById(ID)).willReturn(applicationData)
