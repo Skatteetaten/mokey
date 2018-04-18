73cde136df9928edd793367ad0f90e2c2a5b46d8
Jarle Hansen <jarle.hansen@skatteetaten.no>
2018-04-18 14:24:28 +0200

    simplified objectmapper setup

--- src/main/kotlin/no/skatteetaten/aurora/mokey/ObjectMapperConfigurer.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/ObjectMapperConfigurer.kt
@@ -8,9 +8,8 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 
 fun configureObjectMapper(objectMapper: ObjectMapper): ObjectMapper {
-    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-    objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true)
-    objectMapper.registerModules(JavaTimeModule())
-    objectMapper.registerKotlinModule()
-    return objectMapper
+    return objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            .configure(SerializationFeature.INDENT_OUTPUT, true)
+            .registerModules(JavaTimeModule())
+            .registerKotlinModule()
 }
