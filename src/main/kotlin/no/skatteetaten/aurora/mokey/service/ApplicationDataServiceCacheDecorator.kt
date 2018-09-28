a0b1627c1c793673c15bd81b94a7c02e05cde1c7
Bjarte Stien Karlsen <bjarte.karlsen@skatteetate.no>
2018-09-28 13:53:31 +0200

    return all affiliations if empty

--- src/main/kotlin/no/skatteetaten/aurora/mokey/service/ApplicationDataServiceCacheDecorator.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/service/ApplicationDataServiceCacheDecorator.kt
@@ -24,7 +24,7 @@ class ApplicationDataServiceCacheDecorator(
 ) : ApplicationDataService {
 
     val affiliations: List<String>?
-        get() = if (affiliationsConfig.isBlank()) null
+        get() = if (affiliationsConfig.isBlank()) applicationDataService.findAllAffiliations()
         else affiliationsConfig.split(",").map { it.trim() }
 
     val cache = ConcurrentHashMap<String, ApplicationData>()
