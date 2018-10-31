6d7d1c8f1bc90ae3d57f4ef145eb915791703ec7
Bjarte Stien Karlsen <bjarte.karlsen@skatteetaten.no>
2018-10-31 13:51:55 +0100

    moved duplicate to debug since it is not a problem for new stuff anymore

--- src/main/kotlin/no/skatteetaten/aurora/mokey/service/ApplicationDataServiceOpenShift.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/service/ApplicationDataServiceOpenShift.kt
@@ -110,7 +110,7 @@ class ApplicationDataServiceOpenShift(
             val data = results.mapNotNull { it.applicationData }
             data.groupBy { it.applicationDeploymentId }.filter { it.value.size != 1 }.forEach {
                 val names = it.value.map { "${it.namespace}/${it.applicationDeploymentName}" }
-                logger.info("Duplicate applicationDeploymeentId for=$names")
+                logger.debug("Duplicate applicationDeploymeentId for=$names")
             }
 
             logger.debug("Found deployments=${applicationDeployments.size} data=${data.size} result=${results.size} errors=${errors.size}")
