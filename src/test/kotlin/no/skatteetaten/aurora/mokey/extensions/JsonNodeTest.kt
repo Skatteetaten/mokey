5c24cc188f4872b213840e87f6f693ede0a018d8
Espen Strømland <espen.stromland@skatteetaten.no>
2019-04-09 09:25:58 +0200

    added tests

---
+++ src/test/kotlin/no/skatteetaten/aurora/mokey/extensions/JsonNodeTest.kt
@@ -0,0 +1,38 @@
+package no.skatteetaten.aurora.mokey.extensions
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.junit.Test
+
+class JsonNodeTest {
+    private val mapper = jacksonObjectMapper()
+
+    @Test
+    fun `it should extract value for a given key`() {
+        val git = mapper.readTree(
+            """{
+            |"commit.time": "03.04.2019 @ 16:30:27 CEST",
+            |"commit.id.short":"jd342h4"
+            |}""".trimMargin()
+        )
+
+        val id = git.extract("/commit.id.abbrev", "/commit/id", "/commit.id.short")
+        assertThat(id?.asText()).isEqualTo("jd342h4")
+
+        val time = git.extract("/commit.time/v1", "/commit.time", "/commit/time")
+        assertThat(time?.asText()).isEqualTo("03.04.2019 @ 16:30:27 CEST")
+    }
+
+    @Test
+    fun `it should extract value for keys with exact match`() {
+        val git = mapper.readTree(
+            """{
+            |"commit.time/v1": "03.04.2019 @ 16:30:27 CEST"
+            |}""".trimMargin()
+        )
+
+        val time = git.extract("/commit.time/v1")
+        assertThat(time?.asText()).isEqualTo("03.04.2019 @ 16:30:27 CEST")
+    }
+}
---
+++ src/test/kotlin/no/skatteetaten/aurora/mokey/service/DataParserTest.kt
@@ -0,0 +1,20 @@
+package no.skatteetaten.aurora.mokey.service
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.Test
+
+class DataParserTest {
+
+    @Test
+    fun `it should parse a given time into an instant`() {
+        val instant = DateParser.parseString("03.04.2019 @ 16:17:35 CEST")
+        assertThat(instant?.toString()).isEqualTo("2019-04-03T14:17:35Z")
+
+        val instant1 = DateParser.parseString("2019-04-03 16:17:35 +0200")
+        assertThat(instant1?.toString()).isEqualTo("2019-04-03T14:17:35Z")
+
+        val instant2 = DateParser.parseString("2019-04-03T14:17:35Z")
+        assertThat(instant2?.toString()).isEqualTo("2019-04-03T14:17:35Z")
+    }
+}
