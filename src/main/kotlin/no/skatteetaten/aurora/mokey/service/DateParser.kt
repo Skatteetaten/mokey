8bbe8b03251e2ca6741f334afe27698609048b8c
Espen Strømland <espen.stromland@skatteetaten.no>
2019-04-09 12:20:20 +0200

    added locale uk

--- src/main/kotlin/no/skatteetaten/aurora/mokey/service/DateParser.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/service/DateParser.kt
@@ -8,7 +8,7 @@ import java.util.Locale
 object DateParser {
     val formatters = listOf(
         DateTimeFormatter.ISO_DATE_TIME, // Ex: 2018-03-23T10:53:31Z
-        // Locale must be set to Locale.UK to parse correctly on the server.
+        // Must add Locale.UK to pattern to parse correctly on the server.
         DateTimeFormatter.ofPattern("dd.MM.yyyy '@' HH:mm:ss z", Locale.UK), // Ex: 26.03.2018 @ 13:31:39 CEST
         DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss Z", Locale.UK) // Ex: 2019-03-28 13:26:58 +0100
     )
