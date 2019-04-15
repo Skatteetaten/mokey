298d90f2cbdf2d5b31200c905491c5be307001f2
Espen Strømland <espen.stromland@gmail.com>
2019-04-15 14:55:31 +0200

    feature/aos 1602 se git informasjon i aurora konsoll (#94)
    * wip
    
    * added tests
    
    * removed test openshift config
    
    * added locale uk
    
    * added locale uk

--- src/main/kotlin/no/skatteetaten/aurora/mokey/extensions/JsonNode.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/extensions/JsonNode.kt
@@ -24,6 +24,24 @@ fun JsonNode.extract(vararg pathAlternatives: String): JsonNode? {
         if (valueNode !is MissingNode) {
             break
         }
+
+        valueNode = this.getExact(alt)
+        if (valueNode !is MissingNode) {
+            break
+        }
     }
     return if (valueNode !is MissingNode) valueNode else null
+}
+
+/**
+ * This is for fields that contains slash in key. Ex: "commit.time/v1"
+ * @return value for key if key exists or MissingNode if not.
+ */
+fun JsonNode.getExact(path: String): JsonNode {
+    val value = try {
+        this.fields().asSequence().first { "/${it.key}" == path }?.value
+    } catch (ex: Exception) {
+        null
+    }
+    return value ?: MissingNode.getInstance()
 }
