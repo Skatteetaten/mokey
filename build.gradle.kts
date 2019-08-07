069d2320d1811dd183e7f2259ff46f941479b587
Erlend Haugsnes <Erlend.Haugsnes@skatteetaten.no>
2019-08-07 12:24:01 +0200

    bumped some versions and gradlewrapper to 5.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,17 +7,17 @@ buildscript {
 }
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.30"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.3.30"
-    id("org.jlleitschuh.gradle.ktlint") version "7.3.0"
+    id("org.jetbrains.kotlin.jvm") version "1.3.31"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.31"
+    id("org.jlleitschuh.gradle.ktlint") version "8.0.0"
     id("org.sonarqube") version "2.7.1"
 
-    id("org.springframework.boot") version "2.1.4.RELEASE"
+    id("org.springframework.boot") version "2.1.7.RELEASE"
     id("com.gorylenko.gradle-git-properties") version "2.0.0"
     id("com.github.ben-manes.versions") version "0.21.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.9"
 
-    id("no.skatteetaten.gradle.aurora") version "local-SNAPSHOT"
+    id("no.skatteetaten.gradle.aurora") version "2.4.0"
 }
 
 apply(plugin = "spring-cloud-contract")
