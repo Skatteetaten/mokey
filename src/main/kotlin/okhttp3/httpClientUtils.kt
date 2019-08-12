3655f71c52e9999e11b5b58e6bba75aa32b6afdb
Jarle Hansen <jarle.hansen@skatteetaten.no>
2019-08-12 12:26:52 +0200

    refactor

--- src/main/kotlin/okhttp3/httpClientUtils.kt
+++ src/main/kotlin/okhttp3/httpClientUtils.kt
@@ -3,9 +3,7 @@ package okhttp3
 import io.fabric8.kubernetes.client.ConfigBuilder
 import io.fabric8.kubernetes.client.utils.HttpClientUtils
 
-fun createOpenShiftHttpClient(): OkHttpClient {
-    return OkHttpClient
-        .Builder(HttpClientUtils.createHttpClient(ConfigBuilder().build()))
-        .protocols(listOf(Protocol.HTTP_1_1))
-        .build()
-}
+fun createOpenShiftHttpClient() = OkHttpClient
+    .Builder(HttpClientUtils.createHttpClient(ConfigBuilder().build()))
+    .protocols(listOf(Protocol.HTTP_1_1))
+    .build()
