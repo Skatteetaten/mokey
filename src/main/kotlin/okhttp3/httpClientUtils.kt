b190abb5a6808de854782238ce60c77d995b4498
Jarle Hansen <jarle.hansen@skatteetaten.no>
2019-08-13 08:14:45 +0200

    increase connection and read timeout

--- src/main/kotlin/okhttp3/httpClientUtils.kt
+++ src/main/kotlin/okhttp3/httpClientUtils.kt
@@ -3,7 +3,8 @@ package okhttp3
 import io.fabric8.kubernetes.client.ConfigBuilder
 import io.fabric8.kubernetes.client.utils.HttpClientUtils
 
-fun createOpenShiftHttpClient() = OkHttpClient
-    .Builder(HttpClientUtils.createHttpClient(ConfigBuilder().build()))
-    .protocols(listOf(Protocol.HTTP_1_1))
-    .build()
+fun createOpenShiftHttpClient() = OkHttpClient.Builder(
+    HttpClientUtils.createHttpClient(
+        ConfigBuilder().withConnectionTimeout(60_000).withRequestTimeout(60_000).build()
+    )
+).protocols(listOf(Protocol.HTTP_1_1)).build()
