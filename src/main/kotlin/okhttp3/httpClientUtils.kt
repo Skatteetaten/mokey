b40ee451b719bd30415824b20eb8b294430d627c
Jarle Hansen <jarle.hansen@skatteetaten.no>
2019-08-13 13:30:47 +0200

    lower the OkHttp ConnectionPool size and keep alive duration

--- src/main/kotlin/okhttp3/httpClientUtils.kt
+++ src/main/kotlin/okhttp3/httpClientUtils.kt
@@ -2,9 +2,13 @@ package okhttp3
 
 import io.fabric8.kubernetes.client.ConfigBuilder
 import io.fabric8.kubernetes.client.utils.HttpClientUtils
+import java.util.concurrent.TimeUnit
 
 fun createOpenShiftHttpClient() = OkHttpClient.Builder(
     HttpClientUtils.createHttpClient(
         ConfigBuilder().withConnectionTimeout(60_000).withRequestTimeout(60_000).build()
     )
-).protocols(listOf(Protocol.HTTP_1_1)).build()
+)
+    .protocols(listOf(Protocol.HTTP_1_1))
+    .connectionPool(ConnectionPool(1, 30, TimeUnit.SECONDS))
+    .build()
