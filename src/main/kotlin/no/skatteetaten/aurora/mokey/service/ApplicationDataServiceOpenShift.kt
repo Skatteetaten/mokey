a91ebb4e67a853eba86514787a4ff84fbe6fd45d
Jarle Hansen <jarle.hansen@skatteetaten.no>
2019-08-15 09:33:10 +0200

    added coroutines

--- src/main/kotlin/no/skatteetaten/aurora/mokey/service/ApplicationDataServiceOpenShift.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/service/ApplicationDataServiceOpenShift.kt
@@ -1,6 +1,8 @@
 package no.skatteetaten.aurora.mokey.service
 
 import io.fabric8.openshift.api.model.DeploymentConfig
+import kotlinx.coroutines.async
+import kotlinx.coroutines.newFixedThreadPoolContext
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.slf4j.MDCContext
 import no.skatteetaten.aurora.mokey.extensions.affiliation
@@ -27,6 +29,8 @@ class ApplicationDataServiceOpenShift(
     val addressService: AddressService,
     val imageService: ImageService
 ) {
+    val mtContext = newFixedThreadPoolContext(6, "mokeyPool")
+
     val logger: Logger = LoggerFactory.getLogger(ApplicationDataServiceOpenShift::class.java)
 
     fun findAndGroupAffiliations(affiliations: List<String> = emptyList()): Map<String, List<Environment>> {
@@ -58,7 +62,9 @@ class ApplicationDataServiceOpenShift(
             }
 
             val results = applicationDeployments.map {
-                tryCreateApplicationData(it)
+                async(mtContext) { tryCreateApplicationData(it) }
+            }.map {
+                it.await()
             }
 
             val errors = results.mapNotNull { it.error }
