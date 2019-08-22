ca31a581856db67287e91526ff7cd522ee53db14
Jarle Hansen <jarle.hansen@skatteetaten.no>
2019-08-22 11:56:50 +0200

    refactor

--- src/main/kotlin/no/skatteetaten/aurora/openshift/webclient/OpenShiftUrlBuilder.kt
+++ src/main/kotlin/no/skatteetaten/aurora/openshift/webclient/OpenShiftUrlBuilder.kt
@@ -1,47 +1,14 @@
 package no.skatteetaten.aurora.openshift.webclient
 
-val kubernetesNonApiGroupResources = setOf(
-    "namespace", "service", "configmap", "secret", "serviceaccount",
-    "replicationcontroller", "persistentvolumeclaim", "pod"
-)
-
-fun findOpenShiftApiPrefix(apiVersion: String, kind: String) =
-    if (apiVersion == "v1") {
-        if (kind.toLowerCase() in kubernetesNonApiGroupResources) {
-            "api"
-        } else {
-            "oapi"
-        }
-    } else {
-        "apis"
-    }
-
-val apiGroups: Map<String, List<String>> =
-    mapOf(
-        "skatteetaten.no/v1" to listOf("applicationdeployment"),
-        "apps.openshift.io/v1" to listOf("deploymentconfig", "deploymentrequest"),
-        "route.openshift.io/v1" to listOf("route"),
-        "user.openshift.io/v1" to listOf("user", "group"),
-        "project.openshift.io/v1" to listOf("project", "projectrequest"),
-        "template.openshift.io/v1" to listOf("template"),
-        "image.openshift.io/v1" to listOf("imagestream", "imagestreamtag", "imagestreamimport"),
-        "authorization.openshift.io/v1" to listOf("rolebinding"),
-        "build.openshift.io/v1" to listOf("buildconfig")
-    )
-
 interface ApiGroup {
     fun path(namespace: String? = null, name: String? = null): String
     fun labelSelector(labels: Map<String, String> = emptyMap()) =
         labels.map { "${it.key}=${it.value}" }.joinToString(",")
 }
 
-private fun ns(namespace: String?) = namespace?.let {
-    "/namespaces/$it"
-} ?: ""
+private fun ns(namespace: String?) = namespace?.let { "/namespaces/$it" } ?: ""
 
-private fun n(name: String?) = name?.let {
-    "/$it"
-} ?: ""
+private fun n(name: String?) = name?.let { "/$it" } ?: ""
 
 enum class OpenShiftApiGroup(private val label: String, private val suffix: String = "") : ApiGroup {
     APPLICATIONDEPLOYMENT("skatteetaten.no/v1"),
