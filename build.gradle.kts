18bf556a69939e96cb377481871309e056238cb7
Jarle Hansen <jarle@jarlehansen.net>
2019-09-16 16:16:37 +0200

    Enable nexus iq (#108)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,14 @@ import com.adarshr.gradle.testlogger.theme.ThemeType
 
 plugins {
     id("org.springframework.cloud.contract")
-    id("org.jetbrains.kotlin.jvm") version "1.3.41"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.3.41"
+    id("org.jetbrains.kotlin.jvm") version "1.3.50"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.50"
     id("org.jlleitschuh.gradle.ktlint") version "8.2.0"
     id("org.sonarqube") version "2.7.1"
 
-    id("org.springframework.boot") version "2.1.7.RELEASE"
-    id("com.gorylenko.gradle-git-properties") version "2.0.0"
-    id("com.github.ben-manes.versions") version "0.22.0"
+    id("org.springframework.boot") version "2.1.8.RELEASE"
+    id("com.gorylenko.gradle-git-properties") version "2.1.0"
+    id("com.github.ben-manes.versions") version "0.25.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.12"
     id("com.adarshr.test-logger") version "1.7.0"
 
@@ -19,12 +19,12 @@ plugins {
 extra["spring-hateoas.version"] = "0.24.0.RELEASE"
 
 dependencies {
-    implementation("com.squareup.okhttp3:okhttp:4.1.0")
+    implementation("com.squareup.okhttp3:okhttp:4.2.0")
     implementation("com.fkorotkov:kubernetes-dsl:2.2")
-    testImplementation("io.fabric8:openshift-server-mock:4.4.1")
+    testImplementation("io.fabric8:openshift-server-mock:4.5.1")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.2.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.3.1")
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
