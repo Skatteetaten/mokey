085924f39d2896fcb893aad1ad404ca231d04b6a
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2019-09-19 13:12:10 +0200

    fix plugins asciidoctor

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.spring") version "1.3.50"
     id("org.jlleitschuh.gradle.ktlint") version "8.2.0"
     id("org.sonarqube") version "2.7.1"
-
+    id("org.asciidoctor.convert") version "2.3.0"
     id("org.springframework.boot") version "2.1.8.RELEASE"
     id("com.gorylenko.gradle-git-properties") version "2.1.0"
     id("com.github.ben-manes.versions") version "0.25.0"
