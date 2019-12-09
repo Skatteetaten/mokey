72ac881050baa04ee5c4edba1819f0f104d7d94c
Jarle Hansen <jarle.hansen@skatteetaten.no>
2019-12-09 15:42:17 +0100

    added failure test and test for items type

--- src/test/kotlin/no/skatteetaten/aurora/mokey/service/ImageRegistryServiceTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/mokey/service/ImageRegistryServiceTest.kt
@@ -1,48 +1,60 @@
 package no.skatteetaten.aurora.mokey.service
 
 import assertk.assertThat
+import assertk.assertions.isFailure
+import assertk.assertions.isInstanceOf
 import assertk.assertions.isNotEmpty
 import assertk.assertions.isNotNull
 import assertk.assertions.isTrue
+import no.skatteetaten.aurora.mockmvc.extensions.mockwebserver.execute
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.core.io.ClassPathResource
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
 
-internal class ImageRegistryServiceTest {
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+class ImageRegistryServiceTest {
     private val server = MockWebServer()
     private val webClient = WebClient.create(server.url("/").toString())
     private val imageRegistryClient = ImageRegistryClient(webClient)
     private val imageRegistryService = ImageRegistryService(imageRegistryClient)
-
-    @BeforeEach
-    internal fun setUp() {
-        val manifest = ClassPathResource("manifest.json").file.readText()
-        server.enqueue(
-            MockResponse()
-                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .setBody(manifest)
-        )
-    }
+    private val tagUrls = listOf("localhost/sha256:1", "localhost/sha256:2")
 
     @AfterEach
-    internal fun tearDown() {
+    fun tearDown() {
         server.shutdown()
     }
 
     @Test
-    internal fun `find tags by name`() {
-        val tagUrls =
-            listOf("localhost/sha256:1", "localhost/sha256:2")
+    fun `find tags by name`() {
+        val manifest = ClassPathResource("manifest.json").file.readText()
+        server.execute(
+            MockResponse()
+                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .setBody(manifest)
+        ) {
+            val response = imageRegistryService.findTagsByName(tagUrls)
 
-        val response = imageRegistryService.findTagsByName(tagUrls)
-        assertThat(response).isNotNull()
-        assertThat(response.success).isTrue()
-        assertThat(response.items).isNotEmpty()
+            assertThat(response).isNotNull()
+            assertThat(response.success).isTrue()
+            assertThat(response.items).isNotEmpty()
+
+            val resource = response.items[0]
+            assertThat(resource).isNotNull()
+        }
+    }
+
+    @Test
+    fun `throws IllegalStateException if response is 404`() {
+        server.execute(MockResponse().setResponseCode(404)) {
+            assertThat {
+                imageRegistryService.findTagsByName(tagUrls)
+            }.isFailure().isInstanceOf(IllegalStateException::class)
+        }
     }
 }
