7eeac46d99d101b80677bfc99a5c9531d48144ef
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2020-02-28 13:13:50 +0100

    added headers to proxyGet

--- src/main/kotlin/no/skatteetaten/aurora/mokey/service/OpenShiftManagementClient.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/service/OpenShiftManagementClient.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.reactive.awaitFirst
 import no.skatteetaten.aurora.kubernetes.ClientTypes
 import no.skatteetaten.aurora.kubernetes.KubernetesReactorClient
 import no.skatteetaten.aurora.kubernetes.TargetClient
+import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import java.time.Duration
@@ -13,9 +14,13 @@ import java.time.Duration
 class OpenShiftManagementClient(
     @TargetClient(ClientTypes.SERVICE_ACCOUNT) val client: KubernetesReactorClient
 ) {
-    // TODO: Blir dette feil hvis man har spring boot2 format? Setter vi en header her i master versjonen?
     suspend fun proxyManagementInterfaceRaw(pod: Pod, port: Int, path: String): String {
-        return client.proxyGet<String>(pod, port, path)
+        return client.proxyGet<String>(
+            pod = pod,
+            port = port,
+            path = path,
+            headers = mapOf(HttpHeaders.ACCEPT to "application/vnd.spring-boot.actuator.v2+json,application/json")
+        )
             .timeout(
                 Duration.ofSeconds(2),
                 Mono.error<String>(RuntimeException("Timed out getting management interface in namespace=${pod.metadata.namespace}  pod=${pod.metadata.name} path=$path for "))
