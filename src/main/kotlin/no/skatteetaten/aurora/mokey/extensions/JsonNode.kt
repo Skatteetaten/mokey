0a4b5af446b3c38387d643fa63c6c6eb660243ac
Jarle Hansen <jarle.hansen@skatteetaten.no>
2020-03-19 19:51:06 +0100

    added tests for ImageService, removed unused code

--- src/main/kotlin/no/skatteetaten/aurora/mokey/extensions/JsonNode.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/extensions/JsonNode.kt
@@ -2,20 +2,6 @@ package no.skatteetaten.aurora.mokey.extensions
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.MissingNode
-import com.fasterxml.jackson.databind.node.ObjectNode
-
-fun JsonNode?.asMap(): Map<String, JsonNode> {
-    if (this == null) {
-        return emptyMap()
-    }
-
-    if (this is ObjectNode) {
-        val fields = java.util.HashMap<String, JsonNode>()
-        this.fields().forEachRemaining { entry -> fields[entry.key] = entry.value }
-        return fields
-    }
-    return emptyMap()
-}
 
 fun JsonNode.extract(vararg pathAlternatives: String): JsonNode? {
     var valueNode: JsonNode = MissingNode.getInstance()
