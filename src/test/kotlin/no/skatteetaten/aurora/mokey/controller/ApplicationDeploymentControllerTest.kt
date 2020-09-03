a22770627e1201baad62f694e3b5a36b8d3f0804
Jarle Hansen <jarle@jarlehansen.net>
2020-09-03 16:38:30 +0200

    AOS-4831 added endpoint to get ADs Refs (#145)
    * added endpoint to get ADs Refs
    
    * updated test name
    
    * updated dependencies

--- src/test/kotlin/no/skatteetaten/aurora/mokey/controller/ApplicationDeploymentControllerTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/mokey/controller/ApplicationDeploymentControllerTest.kt
@@ -3,15 +3,19 @@ package no.skatteetaten.aurora.mokey.controller
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import no.skatteetaten.aurora.mockmvc.extensions.Path
+import no.skatteetaten.aurora.mockmvc.extensions.contentTypeJson
 import no.skatteetaten.aurora.mockmvc.extensions.get
+import no.skatteetaten.aurora.mockmvc.extensions.post
 import no.skatteetaten.aurora.mockmvc.extensions.responseJsonPath
 import no.skatteetaten.aurora.mockmvc.extensions.status
 import no.skatteetaten.aurora.mockmvc.extensions.statusIsOk
 import no.skatteetaten.aurora.mokey.AbstractSecurityControllerTest
 import no.skatteetaten.aurora.mokey.ApplicationDataBuilder
 import no.skatteetaten.aurora.mokey.ApplicationDeploymentResourceBuilder
+import no.skatteetaten.aurora.mokey.model.ApplicationDeploymentRef
 import no.skatteetaten.aurora.mokey.service.ApplicationDataService
 import org.junit.jupiter.api.Test
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 
 class ApplicationDeploymentControllerTest : AbstractSecurityControllerTest() {
@@ -29,6 +33,22 @@ class ApplicationDeploymentControllerTest : AbstractSecurityControllerTest() {
 
         mockMvc.get(Path("/api/applicationdeployment/{id}", "123")) {
             statusIsOk().responseJsonPath("$.identifier").equalsValue("123")
+        }
+    }
+
+    @Test
+    fun `Return application deployments by refs`() {
+        every { applicationDataService.findPublicApplicationDataByApplicationDeploymentRef(any()) } returns listOf(
+            ApplicationDataBuilder().build().publicData
+        )
+        every { assembler.toResources(any()) } returns listOf(ApplicationDeploymentResourceBuilder().build())
+
+        mockMvc.post(
+            path = Path("/api/applicationdeployment"),
+            body = listOf(ApplicationDeploymentRef("environment", "application")),
+        headers = HttpHeaders().contentTypeJson()
+        ) {
+            statusIsOk().responseJsonPath("$[0].identifier").equalsValue("123")
         }
     }
 
