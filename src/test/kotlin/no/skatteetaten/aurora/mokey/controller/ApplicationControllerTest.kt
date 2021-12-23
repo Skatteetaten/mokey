303f19d3f7c9208cfe6610cec5876ec1badfd906
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2021-12-23 10:17:07 +0100

    updated stub tests

--- src/test/kotlin/no/skatteetaten/aurora/mokey/controller/ApplicationControllerTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/mokey/controller/ApplicationControllerTest.kt
@@ -9,23 +9,15 @@ import no.skatteetaten.aurora.mokey.service.ApplicationDataService
 import no.skatteetaten.aurora.springboot.AuroraSecurityContextRepository
 import no.skatteetaten.aurora.springboot.OpenShiftAuthenticationManager
 import no.skatteetaten.aurora.springboot.webclient.extensions.kotlin.get
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
-import org.springframework.context.ApplicationContext
-import org.springframework.restdocs.RestDocumentationContextProvider
-import org.springframework.restdocs.RestDocumentationExtension
-import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration
 import org.springframework.security.test.context.support.WithMockUser
-import org.springframework.test.web.reactive.server.WebTestClient
 
-@ExtendWith(RestDocumentationExtension::class)
 @Suppress("unused")
 @WithMockUser("test", roles = ["test"])
 @ExperimentalStdlibApi
 @WebFluxTest(WebSecurityConfig::class, ApplicationController::class)
-class ApplicationControllerTest {
+class ApplicationControllerTest : TestStubSetup() {
     @MockkBean
     private lateinit var openShiftAuthenticationManager: OpenShiftAuthenticationManager
 
@@ -38,16 +30,7 @@ class ApplicationControllerTest {
     @MockkBean
     private lateinit var assembler: ApplicationResourceAssembler
 
-    private lateinit var webTestClient: WebTestClient
-
     private val applicationData = ApplicationDataBuilder().build()
-
-    @BeforeEach
-    fun setUp(applicationContext: ApplicationContext, restDocumentation: RestDocumentationContextProvider) {
-        webTestClient = WebTestClient.bindToApplicationContext(applicationContext).configureClient()
-            .filter(documentationConfiguration(restDocumentation))
-            .build()
-    }
 
     @Test
     fun `Return application by id`() {
@@ -70,17 +53,14 @@ class ApplicationControllerTest {
         every { assembler.toResources(any()) } returns listOf(ApplicationResourceBuilder().build())
 
         webTestClient
-            .get()
-            .uri {
-                it
-                    .path("/api/application")
-                    .queryParam("affiliation", "paas")
-                    .build()
+            .get(path = "/api/application", uriBuilder = {
+                queryParam("affiliation", "paas")
+            }) {
+                exchange()
+                    .expectStatus()
+                    .isOk
+                    .expectBody()
+                    .jsonPath("$.length()").isEqualTo(1)
             }
-            .exchange()
-            .expectStatus()
-            .isOk
-            .expectBody()
-            .jsonPath("$.length()").isEqualTo(1)
     }
 }
--- src/test/kotlin/no/skatteetaten/aurora/mokey/controller/ApplicationDeploymentDetailsResourceAssemblerTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/mokey/controller/ApplicationDeploymentDetailsResourceAssemblerTest.kt
@@ -4,19 +4,10 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isNotNull
 import no.skatteetaten.aurora.mokey.ApplicationDataBuilder
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.mock.web.MockHttpServletRequest
-import org.springframework.web.context.request.RequestContextHolder
-import org.springframework.web.context.request.ServletRequestAttributes
 
 @ExperimentalStdlibApi
 class ApplicationDeploymentDetailsResourceAssemblerTest {
-    @BeforeEach
-    fun setUp() {
-        val request = MockHttpServletRequest()
-        RequestContextHolder.setRequestAttributes(ServletRequestAttributes(request))
-    }
 
     @Test
     fun `Get ApplicationDeploymentDetailsResource with metrics link`() {
