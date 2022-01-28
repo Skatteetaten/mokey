05e41efa9af987d46c922bfd5fc7c2ddc68a4bd0
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2022-01-28 10:19:59 +0100

    tweaked thread pool and number of deployments for pmap

--- src/main/kotlin/no/skatteetaten/aurora/mokey/service/ApplicationDataServiceOpenShift.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/service/ApplicationDataServiceOpenShift.kt
@@ -60,7 +60,7 @@ class ApplicationDataServiceOpenShift(
     suspend fun findAllApplicationDataByEnvironments(
         applicationDeployments: List<ApplicationDeployment>,
     ): List<ApplicationData> {
-        val results = applicationDeployments.chunked(1000).map {
+        val results = applicationDeployments.chunked(500).map {
             applicationDeployments.pmapIO { tryCreateApplicationData(it) }
         }.flatten()
 
