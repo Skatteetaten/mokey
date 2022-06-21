df216a59ba2216da3486799401244883c898de68
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2022-06-21 10:19:36 +0200

    get list of metrics from MetricsEndpoint

--- src/main/kotlin/no/skatteetaten/aurora/mokey/controller/MokeyMetrics.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/controller/MokeyMetrics.kt
@@ -4,6 +4,7 @@ import io.prometheus.client.CollectorRegistry
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation
 import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse
 import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint
+import org.springframework.boot.actuate.metrics.MetricsEndpoint
 import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint
 import org.springframework.boot.actuate.metrics.export.prometheus.TextOutputFormat
 import org.springframework.lang.Nullable
@@ -33,66 +34,15 @@ class ApplicationStatusEndpoint(collectorRegistry: CollectorRegistry) : Promethe
 
 @Component
 @WebEndpoint(id = "prometheus")
-class MokeyMetricsEndpoint(collectorRegistry: CollectorRegistry) : PrometheusScrapeEndpoint(collectorRegistry) {
+class MokeyMetricsEndpoint(collectorRegistry: CollectorRegistry, private val metricsEndpoint: MetricsEndpoint) :
+    PrometheusScrapeEndpoint(collectorRegistry) {
     @ReadOperation(producesFrom = TextOutputFormat::class)
     override fun scrape(
         format: TextOutputFormat?,
         @Nullable includedNames: MutableSet<String>?
-    ): WebEndpointResponse<String> =
-        super.scrape(
-            format,
-            setOf(
-                "application_ready_time",
-                "application_started_time",
-                "disk_free,disk_total",
-                "executor_active",
-                "executor_completed",
-                "executor_pool_core",
-                "executor_pool_max",
-                "executor_pool_size",
-                "executor_queue_remaining",
-                "executor_queued",
-                "jvm_buffer_count",
-                "jvm_buffer_memory_used",
-                "jvm_buffer_total_capacity",
-                "jvm_classes_loaded",
-                "jvm_classes_unloaded",
-                "jvm_gc_live_data_size",
-                "jvm_gc_max_data_size",
-                "jvm_gc_memory_allocated",
-                "jvm_gc_memory_promoted",
-                "jvm_gc_overhead",
-                "jvm_gc_pause",
-                "jvm_memory_committed",
-                "jvm_memory_max",
-                "jvm_memory_usage_after_gc",
-                "jvm_memory_used",
-                "jvm_threads_daemon",
-                "jvm_threads_live",
-                "jvm_threads_peak",
-                "jvm_threads_states",
-                "logback_events",
-                "process_cpu_usage",
-                "process_files_max",
-                "process_files_open",
-                "process_start_time",
-                "process_uptime",
-                "reactor_netty_connection_provider_active_connections",
-                "reactor_netty_connection_provider_idle_connections",
-                "reactor_netty_connection_provider_max_connections",
-                "reactor_netty_connection_provider_max_pending_connections",
-                "reactor_netty_connection_provider_pending_connections",
-                "reactor_netty_connection_provider_total_connections",
-                "system_cpu_count",
-                "system_cpu_usage",
-                "system_load_average_1m",
-                "zipkin_reporter_messages",
-                "zipkin_reporter_messages_total",
-                "zipkin_reporter_queue_bytes",
-                "zipkin_reporter_queue_spans",
-                "zipkin_reporter_spans",
-                "zipkin_reporter_spans_dropped",
-                "zipkin_reporter_spans_total"
-            )
-        )
+    ): WebEndpointResponse<String> {
+        val names = metricsEndpoint.listNames().names
+            .map { it.replace(".", "_") } - listOf("application_info", "application_status")
+        return super.scrape(format, names.toSet())
+    }
 }
