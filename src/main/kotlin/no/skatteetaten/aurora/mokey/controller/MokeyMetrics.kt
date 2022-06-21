0ef137cdf411f5810668c94ef0ca507ba6e9a461
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2022-06-21 09:58:12 +0200

    inherit from PrometheusScrapeEndpoint

--- src/main/kotlin/no/skatteetaten/aurora/mokey/controller/MokeyMetrics.kt
+++ src/main/kotlin/no/skatteetaten/aurora/mokey/controller/MokeyMetrics.kt
@@ -1,33 +1,45 @@
 package no.skatteetaten.aurora.mokey.controller
 
+import io.prometheus.client.CollectorRegistry
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation
+import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse
 import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint
 import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint
 import org.springframework.boot.actuate.metrics.export.prometheus.TextOutputFormat
+import org.springframework.lang.Nullable
 import org.springframework.stereotype.Component
 
 @Component
 @WebEndpoint(id = "prometheus-application-info")
-class ApplicationInfoEndpoint(private val prometheusScrapeEndpoint: PrometheusScrapeEndpoint) {
+class ApplicationInfoEndpoint(collectorRegistry: CollectorRegistry) : PrometheusScrapeEndpoint(collectorRegistry) {
     @ReadOperation(producesFrom = TextOutputFormat::class)
-    fun getApplicationInfo(format: TextOutputFormat) =
-        prometheusScrapeEndpoint.scrape(format, setOf("application_info"))
+    override fun scrape(
+        format: TextOutputFormat?,
+        @Nullable includedNames: MutableSet<String>?
+    ): WebEndpointResponse<String> =
+        super.scrape(format, setOf("application_info"))
 }
 
 @Component
 @WebEndpoint(id = "prometheus-application-status")
-class ApplicationStatusEndpoint(private val prometheusScrapeEndpoint: PrometheusScrapeEndpoint) {
+class ApplicationStatusEndpoint(collectorRegistry: CollectorRegistry) : PrometheusScrapeEndpoint(collectorRegistry) {
     @ReadOperation(producesFrom = TextOutputFormat::class)
-    fun getApplicationStatus(format: TextOutputFormat) =
-        prometheusScrapeEndpoint.scrape(format, setOf("application_status"))
+    override fun scrape(
+        format: TextOutputFormat?,
+        @Nullable includedNames: MutableSet<String>?
+    ): WebEndpointResponse<String> =
+        super.scrape(format, setOf("application_status"))
 }
 
 @Component
-@WebEndpoint(id = "prometheus-mokey-metrics")
-class MokeyMetricsEndpoint(private val prometheusScrapeEndpoint: PrometheusScrapeEndpoint) {
+@WebEndpoint(id = "prometheus")
+class MokeyMetricsEndpoint(collectorRegistry: CollectorRegistry) : PrometheusScrapeEndpoint(collectorRegistry) {
     @ReadOperation(producesFrom = TextOutputFormat::class)
-    fun getMokeyMetrics(format: TextOutputFormat) =
-        prometheusScrapeEndpoint.scrape(
+    override fun scrape(
+        format: TextOutputFormat?,
+        @Nullable includedNames: MutableSet<String>?
+    ): WebEndpointResponse<String> =
+        super.scrape(
             format,
             setOf(
                 "application_ready_time",
